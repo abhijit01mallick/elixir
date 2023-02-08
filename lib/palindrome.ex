eee5b338fd62fa1bce96e283e9c18705cd7143b4
01Abhijit <96957616+01Abhijit@users.noreply.github.com>
2023-02-08 17:57:27 +0530

    palindrome changes

--- lib/palindrome.ex
+++ lib/palindrome.ex
@@ -1,9 +1,23 @@
 defmodule Palindrome do
   def palindrome(number) when number >= 0 do
     digits = number
-      |> Integer.digits()
+      |> to_digits
 
-    digits == Enum.reverse(digits)
+    digits == reverse(digits)
   end
   def palindrome(number) when number < 0, do: false
+
+  def to_digits(number, newlist \\ []) when number > 0 do
+    digit = Integer.mod(number, 10)
+    new_num = div(number,10)
+    to_digits(new_num, [digit | newlist])
+  end
+
+  def to_digits(number, newlist), do: newlist
+
+  def reverse([head | tail], reversed \\ []) do
+    reverse(tail, [head | reversed])
+  end
+
+  def reverse([], reversed), do: reversed
 end
